2e45d16abb0b9dc9a2cdefc1554aa2282907838e
Diamant Lelouette <git@diamant.dev>
2022-01-04 16:58:58 +0100

    Quick fix for progress bar.

--- src/generators/embeds.ts
+++ src/generators/embeds.ts
@@ -10,7 +10,7 @@ const inProgressNumber = (progress: number, total: number) => {
   const percentage2Digits = Math.round(percentage * 100) / 100;
   const bar = '🟩'.repeat(percentage / 10);
   const currentProgress =
-    percentage % 10 <= 5 ? '🟥' : percentage % 10 == 0 ? '' : '🟧';
+    percentage % 10 <= 5 ? (percentage % 10 == 0 ? '' : '🟥') : '🟧';
   const empty = '️️▫️'.repeat(10 - percentage / 10);
   const allBar = bar + currentProgress + empty;
   return { percentage2Digits, allBar };
